6c1a1a8304c2a5112bb11bf345a65fa8b7cbc2ad
Amagash <tiffanysouterre@gmail.com>
2020-09-25 17:41:15 +0000

    again

--- pages/index.vue
+++ pages/index.vue
@@ -96,7 +96,7 @@ export default {
   methods: {
     helloThisIsTheLast () {
       // TODO Feature de la mort
-      //  faire un truc de la mort
+      //  faire un truc de la mort qui tue
       //  labels: enhancement, help wanted
     }
   }
