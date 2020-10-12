63e45dfd24f66b93ee92ef035706848c755e3e22
Amagash <tiffanysouterre@gmail.com>
2020-10-12 18:35:11 +0000

    feature de la mort

--- pages/index.vue
+++ pages/index.vue
@@ -95,8 +95,8 @@ export default {
   },
   methods: {
     helloThisIsTheLast () {
-      // TODO Feature
-      //  faire un truc
+      // TODO Feature de la mort
+      //  faire un truc de la mort qui tue 
       //  labels: enhancement, help wanted
     }
   }
